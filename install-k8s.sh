85c9f4546fdd8d923bc063a500e8d23842d97739
gauravsingh5566 <100135137+gauravsingh5566@users.noreply.github.com>
2023-04-20 12:52:06 +0530

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -13,3 +13,12 @@ sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-ke
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+
+#only for master
+#kubeadm init
+# mkdir -p $HOME/.kube
+ # sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+ # sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#kubectl apply -f https://raw.githubusercontent.com/coreos/f1anne1/master/Documentation/kube-f1anne1.ym1
+#kubectl a 1 -f htt s: // raw. ithubusercontent.com/coreos/f1anne1/master/Documentation/k8s-manifests/kube-f1anne1-rbac. ml
