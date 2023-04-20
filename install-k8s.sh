d0b52bcbe682a260588ffce3db4561b046695fad
gauravsingh5566 <100135137+gauravsingh5566@users.noreply.github.com>
2023-04-20 12:23:43 +0530

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -9,7 +9,7 @@ docker --version
 systemctl start docker
 systemctl enable docker
 
-sudo curl -s https://packages.cloud.google.com/apt... | sudo apt-key add 
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" << /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubelet kubeadm kubectl kubernetes-cni
